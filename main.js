0b53f61cfd54f410f0c613a7bd669624d315babd
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-02-07 16:19:34 -0500

    added opacity selector to click function

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const current = document.querySelector("#current");
 const imgs = document.querySelectorAll(".imgs img");
+const opacity = 0.4;
 
-imgs.forEach(img => img.addEventListener("click", (e) =>
-    current.src = e.target.src
+imgs.forEach(img => img.addEventListener("click",
+    imgClick
 ));
+
+function imgClick(e) {
+    imgs.forEach(img => (img.style.opacity = 1));
+
+    //change current image to source of clicked image
+    current.src = e.target.src;
+
+    //change opacity to opacity const
+    e.target.style.opacity = opacity;
+}
