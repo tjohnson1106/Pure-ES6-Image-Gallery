49db9ae75ae6b6585b4537bf793bcb5afbb0846c
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-02-07 16:28:51 -0500

    added fade in css effect and es6 class and function

--- main.js
+++ main.js
@@ -7,10 +7,19 @@ imgs.forEach(img => img.addEventListener("click",
 ));
 
 function imgClick(e) {
+    //reset opacity
     imgs.forEach(img => (img.style.opacity = 1));
 
     //change current image to source of clicked image
     current.src = e.target.src;
+
+    //add fade-in class
+    current.classList.add("fade-in");
+
+    //remove fade-in class
+    setTimeout(() => current.classList.remove("fade-in"),
+        500
+    );
 
     //change opacity to opacity const
     e.target.style.opacity = opacity;
